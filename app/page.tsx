43042a8f8326db177a45a3d2642e3e5427a8de6b
vhlongon <vhlongon@gmail.com>
2023-08-31 18:55:38 +0200

    update page container styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Form } from '@/components/Form';
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">gradient generator</main>;
+  return (
+    <main className="flex min-h-[100dvh] justify-center flex-col items-center p-24">
+      <Form />
+    </main>
+  );
 }
